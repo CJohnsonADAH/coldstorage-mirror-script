f4f5169c55e395d0643204f26223510a4e349d47
C. Johnson <charlesw.johnson@archives.alabama.gov>
2021-04-20 10:34:34 -0500

    coldstorage-stats-packages-job - allow -Next syntax to do partial counts that cycle through.

--- coldstorage-stats-packages-job.ps1
+++ coldstorage-stats-packages-job.ps1
@@ -1,6 +1,7 @@
 ﻿Param(
     [switch] $Batch=$false,
     [switch] $WhatIf=$false,
+    [Int] $Next=0,
     $Items="",
     $Repository="",
     $Output="CSV",
@@ -37,11 +38,6 @@ If ( $OutputFile -eq $null ) {
     $OutputFile = "${bin}\logs\coldstorage-packages${RepositorySlug}${ItemsSlug}-${Timestamp}.csv"
 }
 
-If ( $Batch ) {
-    $Date = ( Date )
-    Write-Output "Launched${RepositorySlug}: ${Date}" >> "${ShareLogFile}"
-    Copy-Item "${ShareLogFile}" "${LogFile}"
-}
 
 # Example:
 # & coldstorage packages -Items . -Output:CSV -Batch -Recurse -Report -Zipped |Tee-Object "F:\Share\Scripts\logs\coldstorage-digitization-masters-packages-20210305.csv"
@@ -52,12 +48,61 @@ If ( $aItems.Count -gt 0 ) {
     $aRepositoryLocation = $( & coldstorage repository -Repository "${Repository}" )
     Set-Location $aRepositoryLocation["FILE"]
 }
+ElseIf ( $Next -gt 0 ) {
+
+    $aRepositoryLocation = ( & "${ColdStorageScript}" repository "${Repository}" )
+    $aNextLocations = @( )
+    If ( $aRepositoryLocation ) {
+        $StateFile = "${HOME}\Desktop\coldstorage-packages${RepositorySlug}-JobItem.txt"
+        
+        $RepositoryPath = $aRepositoryLocation["FILE"]
+        If ( Test-Path -LiteralPath "${RepositoryPath}" -PathType Container ) {
+
+            $PreviousSection = $( If ( Test-Path -LiteralPath "${StateFile}" -PathType Leaf ) { Get-Content "${StateFile}" } Else { $null } )
+            $Subsections = ( Get-ChildItem -Directory "${RepositoryPath}" | Where { ( $_.Name -ne ".coldstorage" ) -And ( $_.Name -ne "ZIP" ) } )
+
+            $pastIt = $false; $afterLocations = ( $Subsections |% { If ( $_.FullName -ieq $PreviousSection ) { $pastIt = $true } ElseIf ( $pastIt ) { $_ } } )
+            $pastIt = $false; $beforeLocations = ( $Subsections |% { If ( $_.FullName -ieq $PreviousSection ) { $pastIt = $true } ElseIf ( -Not $pastIt ) { $_ } } )
+            $afterBefore = @( $afterLocations ) + @( $beforeLocations )
+            
+            $aNextLocations = ( $afterBefore | Select-Object -First ${Next} ).FullName
+
+        }
+
+    }
+
+    If ( $aNextLocations.Count -gt 0 ) {
+        $aNextLocations | Select-Object -First 1 |% { "[coldstorage-stats-packages-job] Next location: {0}" -f $_ } | Write-Warning
+
+        If ( "${StateFile}" ) {
+            ($aNextLocations | Select-Object -Last 1) > "${StateFile}"
+        }
+
+        $bItems = $true
+        $sLocationSwitch = ($aNextLocations -join " ")
+        Set-Location $RepositoryPath
+
+        $ItemsSlug = ( "-${sLocationSwitch}" -replace "[^0-9A-Za-z_]+","-" )
+        $LogFile = "${HOME}\Desktop\coldstorage-packages${RepositorySlug}${ItemsSlug}-log.txt"
+        $ShareLogFile = "${bin}\coldstorage-packages${RepositorySlug}${ItemsSlug}-log.txt"
+
+    }
+    Else {
+        ( "[coldstorage-stats-packages-job] Could not locate repository {0} or list for items." -f "${Repository}" )
+        Exit
+    }
+
+}
 Else {
     $bItems = $false
     $sLocationSwitch = "${Repository}"
 }
 
 If ( $Batch ) {
+
+    $Date = ( Date )
+    Write-Output "Launched${RepositorySlug}: ${Date}" >> "${ShareLogFile}"
+    Copy-Item "${ShareLogFile}" "${LogFile}"
     
     $t0 = ( Get-Date )
     
