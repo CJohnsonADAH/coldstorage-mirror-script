a70c3ef17a6e0ffdc956781b0e369428e6cb8181
C. Johnson <charlesw.johnson@archives.alabama.gov>
2023-02-06 14:42:52 -0600

    Check in modules missing from repo.

---
+++ coldstorage-stats-job.ps1
@@ -0,0 +1,190 @@
+﻿$ColdStorageER = "\\ADAHColdStorage\ADAHDATA\ElectronicRecords"
+$ColdStorageDA = "\\ADAHColdStorage\ADAHDATA\Digitization"
+$ColdStorageBackup = "\\ADAHColdStorage\Share\ColdStorageMirroredBackup"
+
+$mirrors = @{
+    Processed=( "ER", "\\ADAHFS3\Data\Permanent", "${ColdStorageER}\Processed" )
+    Working_ER=( "ER", "${ColdStorageER}\Working-Mirror", "\\ADAHFS3\Data\ArchivesDiv\PermanentWorking" )
+    Unprocessed=( "ER", "\\ADAHFS1\PermanentBackup\Unprocessed", "${ColdStorageER}\Unprocessed" )
+    Masters=( "DA", "${ColdStorageDA}\Masters", "\\ADAHFS3\Data\DigitalMasters" )
+    Access=( "DA", "${ColdStorageDA}\Access", "\\ADAHFS3\Data\DigitalAccess" )
+    Working_DA=( "DA", "${ColdStorageDA}\Working-Mirror", "\\ADAHFS3\Data\DigitalWorking" )
+}
+
+$Processed = $mirrors["Processed"]
+$Unprocessed = $mirrors["Unprocessed"]
+$Masters = $mirrors["Masters"]
+
+Function ColdStorage-Location {
+Param ( $Repository )
+
+    $aRepo = $mirrors[$Repository]
+
+    If ( ( $aRepo[1] -Like "${ColdStorageER}\*" ) -Or ( $aRepo[1] -Like "${ColdStorageDA}\*" ) ) {
+        $aRepo[1]
+    }
+    Else {
+        $aRepo[2]
+    }
+}
+
+Function ColdStorage-Zip-Location {
+Param ( $Repository )
+
+    $BaseDir = ColdStorage-Location -Repository $Repository
+
+    If ( Test-Path -LiteralPath "${BaseDir}\ZIP" ) {
+        Get-Item -Force -LiteralPath "${BaseDir}\ZIP"
+    }
+}
+
+Function Get-File-Object ( $File ) {
+    
+    $oFile = $null
+    If ( $File -is [String] ) {
+        If ( Test-Path -LiteralPath "${File}" ) {
+            $oFile = ( Get-Item -Force -LiteralPath "${File}" )
+        }
+    }
+    Else {
+        $oFile = $File
+    }
+
+    $oFile
+}
+
+function Is-BagIt-Formatted-Directory ( $File ) {
+    $result = $false # innocent until proven guilty
+
+    $oFile = Get-File-Object -File $File   
+
+    $BagDir = $oFile.FullName
+    if ( Test-Path -LiteralPath $BagDir -PathType Container ) {
+        $PayloadDir = "${BagDir}\data"
+        if ( Test-Path -LiteralPath $PayloadDir -PathType Container ) {
+            $BagItTxt = "${BagDir}\bagit.txt"
+            if ( Test-Path -LiteralPath $BagItTxt -PathType Leaf ) {
+                $result = $true
+            }
+        }
+    }
+
+    return $result
+}
+
+Function Is-Zipped-Bag {
+
+Param ( $LiteralPath )
+
+    $oFile = Get-File-Object -File $LiteralPath
+
+    ( ( $oFile -ne $null ) -and ( $oFile.Name -like '*_md5_*.zip' ) )
+}
+
+function Is-Indexed-Directory {
+Param( $File )
+
+    $FileObject = Get-File-Object($File)
+    $FilePath = $FileObject.FullName
+
+    $result = $false
+    if ( Test-Path -LiteralPath "${FilePath}" ) {
+        $NewFilePath = "${FilePath}\index.html"
+        $result = Test-Path -LiteralPath "${NewFilePath}"
+    }
+    
+    $result
+}
+
+function Is-Bagged-Indexed-Directory {
+Param( $File )
+
+    $FileObject = Get-File-Object($File)
+    $FilePath = $FileObject.FullName
+
+    $result = $false
+    if ( Is-BagIt-Formatted-Directory($File) ) {
+        $payloadPath = "${FilePath}\data"
+        $result = Is-Indexed-Directory($payloadPath)
+    }
+    
+    $result
+}
+
+Function Get-Bagged-ChildItem-Candidates {
+Param( $LiteralPath=$null, $Path=$null, [switch] $Zipped=$false )
+
+    If ( $Zipped ) {
+        If ( $LiteralPath -ne $null ) {
+            $Zips = Get-ChildItem -LiteralPath $LiteralPath -File | Select-Zipped-Bags
+        }
+        Else {
+            $Zips = Get-ChildItem -Path $Path -File | Select-Zipped-Bags
+        }
+        $Zips
+    }
+
+    If ( $LiteralPath -ne $null ) {
+        $Dirs = Get-ChildItem -LiteralPath $LiteralPath -Directory
+    }
+    Else {
+        $Dirs = Get-ChildItem -Path $Path -Directory
+    }
+    $Dirs
+}
+
+Function Get-Bagged-ChildItem {
+Param( $LiteralPath=$null, $Path=$null, [switch] $Zipped=$false )
+
+    Get-Bagged-ChildItem-Candidates -LiteralPath:$LiteralPath -Path:$Path -Zipped:$Zipped |% {
+
+        $Item = Get-File-Object -File $_
+    Write-Progress -Id 101 -Activity "Scanning ${sRepo} for bags" -Status $Item.FullName -PercentComplete 75
+
+        If ( Is-BagIt-Formatted-Directory($Item) ) {
+            $Item
+        }
+        ElseIf ( Is-Zipped-Bag $Item ) {
+            $Item
+        }
+        ElseIf ( Is-Indexed-Directory -File $Item ) {
+            # NOOP - Do not descend into an unbagged indexed directory
+        }
+        ElseIf ( Test-Path -LiteralPath $Item.FullName -PathType Container ) {
+            # Descend to next directory level
+            Get-Bagged-ChildItem -LiteralPath $Item.FullName
+        }
+    }
+
+}
+
+"Processed","Unprocessed", "Masters" |% {
+
+    Write-Progress -Id 101 -Activity "Scanning ${sRepo}"
+    $sRepo = $_
+
+    Write-Progress -Id 101 -Activity "Scanning ${sRepo}" -Status "Getting locations" -PercentComplete 0
+
+    Write-Progress -Id 101 -Activity "Scanning ${sRepo}" -Status "Counting zipped bags" -PercentComplete 50
+
+    $ZipLocation = ( ColdStorage-Zip-Location -Repository $_ )
+    If ( $ZipLocation ) {
+        $nZipped = ( Get-ChildItem -LiteralPath $ZipLocation.FullName ).Count
+    }
+    Else {
+        $nZipped = 0
+    }
+
+    Write-Progress -Id 101 -Activity "Scanning ${sRepo}" -Status "Counting bags" -PercentComplete 75
+    
+    $Location = ( Get-Item -Force -LiteralPath ( ColdStorage-Location -Repository $_ ) )
+
+    $nBagged = ( Get-Bagged-ChildItem -LiteralPath $Location.FullName ).Count
+
+    Write-Progress -Id 101 -Activity "Scanning ${sRepo}" -Status "Writing object" -PercentComplete 100
+   
+    @{} | Select-Object @{n='Location';e={ $sRepo }}, @{n='Bagged';e={ $nBagged }}, @{n='Zipped';e={ $nZipped }}
+
+    Write-Progress -Id 101 -Activity "Scanning ${sRepo}" -Status "Writing object" -PercentComplete 100 -Completed
+   
+}
---
+++ coldstorage-zip-packages.ps1
@@ -0,0 +1,333 @@
+﻿<#
+.SYNOPSIS
+ADAHColdStorage Digital Preservation Packages compression script
+@version 2022.0901
+
+.PARAMETER Skip
+coldstorage zip -Skip allows you to bypass potentially time-consuming steps in the process, like clamav scans, bagit validation, and zip checksum validation. Usually you shouldn't. They're important.
+
+.PARAMETER Batch
+coldstorage mirror -Batch formats output for log files and channels it to easily redirectable stdout, error and warning output streams. Ideal for tasks run from Task Scheduler.
+
+.DESCRIPTION
+coldstorage mirror: Sync files to or from the ColdStorage server.
+coldstorage bag: Package files into BagIt-formatted preservation packages
+coldstorage zip: Zip preservation packages into cloud storage-formatted archival units
+coldstorage to: send a preservation package or an archival unit to external preservation sites (cloud, drop, ...)
+coldstorage vs: compare the packages preserved locally to those stored on an external preservation site (cloud, drop, etc.)
+coldstorage check: Check for new preservation items in a repository to be packaged, and check the status of already-processed preservation packages
+coldstorage stats: Compile statistics on the preservation packages in a repository
+coldstorage packages: Output a report on the preservation packages in a repository
+coldstorage validate: Validate BagIt-formatted preservation packages or cloud storage-formatted archival units
+#>
+
+Using Module ".\ColdStorageProgress.psm1"
+
+Param (
+    [switch] $Development = $false,
+    [switch] $Production = $false,
+    [switch] $Help = $false,
+    [switch] $Quiet = $false,
+	[switch] $Batch = $false,
+    [switch] $Interactive = $false,
+    [String[]] $Skip = @(), 
+    [switch] $Recurse = $false,
+    [switch] $Force = $false,
+    [switch] $PassThru = $false,
+    [switch] $Dev = $false,
+    [switch] $Bork = $false,
+    [switch] $WhatIf = $false,
+    [switch] $Version = $false,
+    [Parameter(ValueFromRemainingArguments=$true, Position=0)] $Words,
+    [Parameter(ValueFromPipeline=$true)] $Piped
+)
+
+$Verbose = ( $PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent )
+$Verbose = $( If ( $Verbose -eq $null ) { $false } Else { $Verbose } )
+$Debug = ( $PSCmdlet.MyInvocation.BoundParameters["Debug"].IsPresent )
+$Debug = $( If ( $Debug -eq $null ) { $false } Else { $Debug } )
+
+$global:gBucketObjects = @{ }
+
+Function Test-CSDevelopmentBranchDir {
+Param ( [Parameter(ValueFromPipeline=$true)] $Item=$null )
+
+    Process {
+        ( $Item.Name -like "*-development" )
+    }
+
+}
+Function Get-CSProductionBranchDirName {
+Param ( [Parameter(ValueFromPipeline=$true)] $Item=$null )
+
+    Process {
+        $Item.Parent.FullName | Join-Path -ChildPath ( $Item.Name -replace "-development$", "" )
+    }
+
+}
+Function Get-CSDevelopmentBranchDirName {
+Param ( [Parameter(ValueFromPipeline=$true)] $Item=$null )
+
+    Process {
+        $Item.Parent.FullName | Join-Path -ChildPath ( "{0}-development" -f ( $Item.Name -replace "-development$","" ) )
+    }
+
+}
+
+Function Get-CSProductionBranchDir {
+Param ( $File=$null )
+
+    $ScriptPath = Get-Item -Force -LiteralPath ( Split-Path -Parent $PSCommandPath )
+    $ScriptPath = $( If ($ScriptPath | Test-CSDevelopmentBranchDir) { $ScriptPath | Get-CSProductionBranchDirName } Else { $ScriptPath.FullName } )
+
+    If ( $File -ne $null ) {
+        ( Get-Item -Force -LiteralPath ( $ScriptPath | Join-Path -ChildPath $File ) )
+    }
+    Else {
+        ( Get-Item -Force -LiteralPath ( $ScriptPath ) )
+    }
+
+}
+
+Function Get-CSDevelopmentBranchDir {
+Param ( $File=$null )
+
+    $ScriptPath = Get-Item -Force -LiteralPath ( Split-Path -Parent $PSCommandPath )
+    $ScriptPath = $( If ($ScriptPath | Test-CSDevelopmentBranchDir) { $ScriptPath.FullName } Else { $ScriptPath | Get-CSDevelopmentBranchDirName } )
+
+    If ( $File -ne $null ) {
+        ( Get-Item -Force -LiteralPath ( $ScriptPath | Join-Path -ChildPath $File ) )
+    }
+    Else {
+        ( Get-Item -Force -LiteralPath ( $ScriptPath ) )
+    }
+
+}
+
+# Do we need to hand off control to an alternate branch of the script?
+If ( $Development -or $Production ) {
+    $ps1 = $( If ($Development) { ( Get-CSDevelopmentBranchDir -File ( Split-Path -Leaf $PSCommandPath ) ) } Else { ( Get-CSProductionBranchDir -File ( Split-Path -Leaf $PSCommandPath ) ) } )
+    $Branch = $( If ( $Development ) { "Development" } Else { "Production" } )
+    If ( $ps1 ) {
+
+        $aParameters = $MyInvocation.BoundParameters
+        $aParameters[$Branch] = $False
+        Write-Warning ( "[{0}] Invoking the {1} branch {2}" -f  $MyInvocation.MyCommand.Name, $Branch, $ps1.FullName )
+        & ( "{0}" -f $ps1.FullName ) @aParameters
+       
+    }
+    Else {
+        Write-Warning ( "[{0}] Could not find {1} branch for {2}" -f $MyInvocation.MyCommand.Name, $Branch, $PSCommandPath )
+    }
+    Exit
+}
+
+Function ColdStorage-Script-Dir {
+Param ( $File=$null )
+
+    $ScriptPath = ( Split-Path -Parent $PSCommandPath )
+    
+    If ( $File -ne $null ) {
+        $Item = ( Get-Item -Force -LiteralPath "${ScriptPath}\${File}" )
+    }
+    Else {
+        $Item = ( Get-Item -Force -LiteralPath $ScriptPath )
+    }
+
+    $Item
+}
+
+# Internal Dependencies - Modules
+$bVerboseModules = ( $Debug -eq $true )
+$bForceModules = ( ( $Debug -eq $true ) -or ( $psISE ) )
+
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageInteraction.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageSettings.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageFiles.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageRepositoryLocations.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStoragePackagingConventions.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageScanFilesOK.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageBagItDirectories.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageBaggedChildItems.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageStats.psm1" )
+Import-Module -Verbose:$bVerboseModules -Force:$bForceModules $( ColdStorage-Script-Dir -File "ColdStorageZipArchives.psm1" )
+
+$global:gCSScriptName = $MyInvocation.MyCommand
+$global:gCSScriptPath = $MyInvocation.MyCommand.Definition
+
+If ( $global:gScriptContextName -eq $null ) {
+    $global:gScriptContextName = $global:gCSScriptName
+}
+
+$ColdStorageER = "\\ADAHColdStorage\ADAHDATA\ElectronicRecords"
+$ColdStorageDA = "\\ADAHColdStorage\ADAHDATA\Digitization"
+
+Function Get-CurrentLine {
+    $MyInvocation.ScriptLineNumber
+}
+
+Function Get-CSGPCommandWithVerb {
+    $global:gCSGPCommandWithVerb
+}
+
+#############################################################################################################
+## COMMAND FUNCTIONS ########################################################################################
+#############################################################################################################
+
+Function Compress-CSBaggedPackage {
+Param( [Parameter(ValueFromPipeline=$true)] $File, $Batch = $false, [String[]] $Skip=@() )
+
+Begin { }
+
+Process {
+    
+    $Progress = [CSProgressMessenger]::new( -Not $Batch, $Batch )
+    $Progress.Open( ( "Processing {0}" -f "${sArchive}" ), "Validating bagged preservation package", 5 )
+
+    If ( Test-BagItFormattedDirectory -File $File ) {
+        $oFile = Get-FileObject -File $File
+        $sFile = Get-FileLiteralPath -File $File
+
+        $Validated = ( Test-CSBaggedPackageValidates -DIRNAME $sFile -Skip:$Skip )
+
+        $Progress.Update( "Validated bagged preservation package" )
+        
+        If ( $Validated | Test-CSOutputForValidationErrors | Test-ShallWeContinue ) {
+
+            $oZip = ( Get-ZippedBagOfUnzippedBag -File $oFile )
+
+            $Result = $null
+
+            If ( $oZip.Count -gt 0 ) {
+                $sArchiveHashed = $oZip.FullName
+                $Result = [PSCustomObject] @{ "Bag"=$sFile; "Zip"="${sArchiveHashed}"; "New"=$false; "Validated"=$Validated; "Compressed"=$null }
+                $Progress.Update( "Located archive with MD5 Checksum", 2 )
+            }
+            Else {
+                $Progress.Update( "Compressing archive" )
+
+                $oRepository = ( $oFile | Get-ZippedBagsContainer )
+                $sRepository = $oRepository.FullName
+
+                $ts = $( Date -UFormat "%Y%m%d%H%M%S" )
+                $sZipPrefix = ( Get-ZippedBagNamePrefix -File $oFile )
+
+                $sZipName = "${sZipPrefix}_z${ts}"
+
+                If ( $sRepository ) {
+                    $sArchive = ( $sRepository | Join-Path -ChildPath "${sZipName}.zip" )
+
+                    $CompressResult = ( $sFile | Compress-ArchiveWith7z -WhatIf:$WhatIf -DestinationPath ${sArchive} )
+
+                    $Progress.Update( "Computing MD5 checksum" )
+                    If ( -Not $WhatIf ) {
+                        $md5 = $( Get-FileHash -LiteralPath "${sArchive}" -Algorithm MD5 ).Hash.ToLower()
+                    }
+                    Else {
+                        $stream = [System.IO.MemoryStream]::new()
+                        $writer = [System.IO.StreamWriter]::new($stream)
+                        $writer.write($sArchive)
+                        $writer.Flush()
+                        $stream.Position = 0
+                        $md5 = $( Get-FileHash -InputStream $stream ).Hash.ToLower()
+                    }
+
+                    $sZipHashedName = "${sZipName}_md5_${md5}"
+                    $sArchiveHashed = ( $sRepository | Join-Path -ChildPath "${sZipHashedName}.zip" )
+
+                    If ( -Not $WhatIf ) {
+                        Move-Item -WhatIf:$WhatIf -LiteralPath $sArchive -Destination $sArchiveHashed
+                    }
+
+                    $Result = [PSCustomObject] @{ "Bag"=$sFile; "Zip"="${sArchiveHashed}"; "New"=$true; "Validated-Bag"=$Validated; "Compressed"=$CompressResult }
+                }
+                Else {
+                    ( "[Compress-CSBaggedPackage] Could not determine destination container for path: '{0}'" -f $oFile.FullName ) | Write-Warning
+                }
+
+            }
+            
+            $Progress.Update( "Testing zip file integrity" )
+
+            If ( $Result -ne $null ) {
+                $Result | Add-Member -MemberType NoteProperty -Name "Validated-Zip" -Value ( Test-ZippedBagIntegrity -File $sArchiveHashed -Skip:$Skip )
+                $Result | Write-Output
+            }
+
+        }
+    }
+    Else {
+        $sFile = $File.FullName
+        Write-Warning "${sFile} is not a BagIt-formatted directory."
+    }
+
+    $Progress.Complete()
+
+}
+
+End { }
+
+}
+
+
+Function Get-PluralizedText {
+Param ( [Parameter(Position=0)] $N, [Parameter(ValueFromPipeline=$true)] $Singular, $Plural="{0}s" )
+
+    Begin { }
+
+    Process {
+        $Pluralized = $( $Plural -f $Singular )
+        If ( $N -eq 1 ) {
+            $Singular
+        }
+        Else {
+            $Pluralized
+        }
+    }
+
+    End { }
+
+}
+
+$sCommandWithVerb = ( $MyInvocation.MyCommand |% { "$_" } )
+$global:gCSCommandWithVerb = $sCommandWithVerb
+
+If ( $Verbose ) {
+    $VerbosePreference = "Continue"
+}
+
+If ( $Help -eq $true ) {
+    Get-CSUsageNotes -cmd $MyInvocation.MyCommand
+}
+ElseIf ( $Version ) {
+    Get-CSScriptVersion -Verb:$Verb -Words:$Words -Flags:$MyInvocation.BoundParameters | Write-Output
+}
+Else {
+    $t0 = date
+    $sCommandWithVerb = "${sCommandWithVerb} ${Verb}"
+    $global:gCSGPCommandWithVerb = $sCommandWithVerb
+
+    If ( $Verb.Length -gt 0 ) {
+        $global:gScriptContextName = $sCommandWithVerb
+    }
+
+    $allObjects = ( @( $Words | Where { $_ -ne $null } ) + @( $Input | Where { $_ -ne $null } ) )
+    $allObjects |% {
+        $sFile = Get-FileLiteralPath -File $_
+        If ( Test-BagItFormattedDirectory -File $sFile ) {
+            $_ | Compress-CSBaggedPackage -Skip:$Skip
+        }
+        ElseIf ( Test-LooseFile -File $_ ) {
+            $oBag = ( Get-BaggedCopyOfLooseFile -File $_ )
+            If ($oBag) {
+                $oBag | Compress-CSBaggedPackage -Skip:$Skip
+            }
+            Else {
+                Write-Warning "${sFile} is a loose file not a BagIt-formatted directory."
+            }
+        }
+        Else {
+            $_ | Get-Item -Force |% { Get-BaggedChildItem -LiteralPath $_.FullName } | Compress-CSBaggedPackage -Skip:$Skip
+        }
+    }
+}
