b9f54810a5b00f540ea9ccf90f3026b3a430ba07
Charles Johnson <charlesw.johnson@archives.alabama.gov>
2026-07-06 13:56:16 -0500

    Server management - current (summer 2026) nightly maintenance reporting and data transfer loops encoded in a script. Should be developed and superseded over time.

---
+++ cs-preservation-window-manager.ps1
@@ -0,0 +1,5 @@
+﻿ $Repos=@( "Masters@2", "Access@7", "Unprocessed", "Masters@4", "Processed", "Masters@10", "Access", "Masters@21", "Unprocessed", "Access@7", "Masters@60", "Processed", "Access", "Masters@365" )
+ & wait-maintenanceschedule.ps1 -Label:"Daily Mirror Data Transfer" -Loop -Job:{
+    invoke-repositories-daily-mirror-cs.ps1 -Repositories:$Repos -Schedule:$null
+    Sleep 10
+} -By:"6:00 PM"
---
+++ cs-report-window-manager.ps1
@@ -0,0 +1,16 @@
+﻿& wait-maintenanceschedule.ps1 -By:"10:00 PM" -Label:"DA-Masters: Q / SC / AMG / WSFA / Digitized_newspapers / Legislative_publications / NAGPRA / Genealogy_history / Executive_orders / Robinson DigitalMasters reporting" -Job:{
+    cd H:\Holdings\Digitization\Masters
+    Get-Item .\Bryan_Carter_photos,
+        .\AMG\Discs,
+        .\Digitized_newspapers,
+        .\Genealogy_history,
+        .\Goldstar,
+        .\Legislative_publications,
+        .\NAGPRA,
+        .\Robinson -Force | out-321preservationreport.ps1 -Attn -Bags -Summary
+    Get-Item .\WSFA,
+        .\Q_numbers,
+        .\Supreme_Court,
+        .\WPA,
+        .\Executive_orders -Force | out-321preservationreport.ps1 -Attn -Candidates -Summary
+} -Loop
